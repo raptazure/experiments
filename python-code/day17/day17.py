f21081d803919eea1ea6689456ec05296742e2a8
yafeng19 <15893009558@qq.com>
2020-02-05 12:16:13 +0800

    update

--- python-code/day17/day17.py
+++ python-code/day17/day17.py
@@ -33,7 +33,7 @@ def sum_all(*args):
     s = 0
     for i in args:
         s += i
-    return s 
+    return s
 print(sum_all(1, 2, 3))             # 6
 print(sum_all(1, 2, 3, 4, 5, 6, 7)) # 28
 
--- python-code/day17/exercises.py
+++ python-code/day17/exercises.py
@@ -1,3 +1,4 @@
-names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
-*nordic_countries,es,ru = names
-print(nordic_countries,es,ru) 
+names = ['Finland', 'Sweden', 'Norway',
+         'Denmark', 'Iceland', 'Estonia', 'Russia']
+*nordic_countries, es, ru = names
+print(nordic_countries, es, ru)
