b3d30eecce696a943ababb8adeb62a68b6f34871
Raptazure <1051276278@qq.com>
2020-03-01 17:51:21 +0800

    vue demo: skills-list

---
+++ vue/skills-list/src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, This is an about page</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nemo et
+      excepturi ea omnis dicta modi iure odio voluptate illo quaerat quos quas,
+      velit perferendis nisi nihil explicabo ad enim?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
---
+++ vue/skills-list/src/components/Skills.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="skills">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input
+          type="text"
+          placeholder="Enter a skill you have..."
+          v-model="skill"
+          v-validate="'min: 5'"
+          name="skill"
+        />
+        <transition
+          name="alert-in"
+          enter-active-class="animated flipInX"
+          leave-active-class="animated flipOutX"
+        >
+          <p class="alert" v-if="errors.has('skill')">
+            {{ errors.first("skill") }}
+          </p>
+        </transition>
+      </form>
+      <ul>
+        <transition-group
+          name="list"
+          enter-active-class="animated bounceInUp"
+          leave-active-class="animated bounceOutDown"
+        >
+          <li v-for="(skill, index) in skills" :key="index">
+            {{ index }}. {{ skill.name }}
+            <i class="fa fa-minus-circle" @click="remove(index)"></i>
+          </li>
+        </transition-group>
+      </ul>
+
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skill: "",
+      skills: [{ name: "Vue.js" }, { name: "Frontend" }]
+    };
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then(res => {
+        if (res) {
+          this.skills.push({ name: this.skill });
+          this.skill = "";
+        } else {
+          console.log("Not Valid");
+        }
+      });
+    },
+    remove(id) {
+      this.skills.splice(id, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
+.holder {
+  background: #fff;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
+.alert-in-enter-active {
+  animation: bounce-in 0.5s;
+}
+.alert-in-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+i {
+  float: right;
+  cursor: pointer;
+}
+</style>
