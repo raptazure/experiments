337b5c0b40128813febccd1aec97e845baddb71d
Raptazure <1051276278@qq.com>
2020-03-10 11:48:07 +0800

    birthday: 3.9

---
+++ typescript/src/19.ts
@@ -0,0 +1,23 @@
+const happyBirthday = new Promise<string> ((pisces, feelings) => {
+  pisces('creation and intuition...')
+})
+  .then(heart => {
+    console.log(heart)
+    return 'Look inside with lucidity...'
+  })
+  .then(past => {
+    console.log(past)
+    return 'Cherish what you have...'
+  })
+  .then(present => {
+    console.log(present)
+    return 'Carry on what you love...'
+  })
+  .then(future => {
+    console.log(future)
+    return '∞'
+  })
+  .catch(sadness => {
+    return 'Just let it go...'
+  })
+  
