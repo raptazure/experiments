319117e6e35ae8a00f43cf79f78c71c7259bf75a
raptazure <1051276278@qq.com>
2020-06-19 22:50:36 +0800

    feat: ownership

---
+++ rust/ownership/src/return_values.rs
@@ -0,0 +1,45 @@
+pub fn return_values_and_scope() {
+    let _s1 = gives_ownership(); // gives_ownership moves its return
+                                 // value into s1
+
+    let s2 = String::from("hello"); // s2 comes into scope
+
+    let _s3 = takes_and_gives_back(s2); // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+    return_multiple_values(); // using tuple to avoid passing in and passing back
+} // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
+  // moved, so nothing happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
+
+    a_string // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
+
+fn return_multiple_values() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}", s2, len);
+}
