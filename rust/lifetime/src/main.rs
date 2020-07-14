c0ce1ee7ed2b6e3cc9138c1f416029d3fd96d793
raptazure <1051276278@qq.com>
2020-07-14 19:20:36 +0800

    feat: lifetime

---
+++ rust/lifetime/src/main.rs
@@ -0,0 +1,53 @@
+use std::fmt::Display;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str 
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        let long_ann = longest_with_an_announcement(string1.as_str(), string2.as_str(), "ann");
+        println!("The longest string is {}, {}", result, long_ann);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{}", i.announce_and_return_part("hi"));
+
+    // let s: &'static str = "I have a static lifetime.";
+
+
+}
