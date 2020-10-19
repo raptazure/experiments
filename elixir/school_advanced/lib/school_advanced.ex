4479b5abdddf5ebddd877df9c94d59fc54f68d77
raptazure <raptazure@foxmail.com>
2020-10-20 00:13:09 +0800

    elixir: using erlang

---
+++ elixir/school_advanced/lib/school_advanced.ex
@@ -0,0 +1,18 @@
+defmodule SchoolAdvanced do
+  @moduledoc """
+  Documentation for `SchoolAdvanced`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SchoolAdvanced.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
