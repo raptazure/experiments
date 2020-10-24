ea563379190c1a79cb356538d39bc8eac1875e71
raptazure <raptazure@foxmail.com>
2020-10-24 08:46:01 +0800

    elixir: specs and types

--- elixir/machine_learning_toolkit/apps/utilities/lib/utilities.ex
+++ elixir/machine_learning_toolkit/apps/utilities/lib/utilities.ex
@@ -1,18 +1,23 @@
 defmodule Utilities do
-  @moduledoc """
-  Documentation for `Utilities`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Utilities.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  # @spec sum_times(integer, %Examples{integer, integer}) :: integer
+  @spec sum_times(integer, Examples.t()) :: integer
+  def sum_times(a, params) do
+    for i <- params.first..params.last do
+      i
+    end
+    |> Enum.map(fn el -> el * a end)
+    |> Enum.sum()
+    |> round
   end
+end
+
+defmodule Example do
+  defstruct first: nil, last: nil
+
+  @type t(first, last) :: %Example{first: first, last: last}
+
+  @typedoc """
+      Type that represents Examples struct with :first as integer and :last as integer.
+  """
+  @type t :: %Example{first: integer, last: integer}
 end
