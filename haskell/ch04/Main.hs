295082b71d8b7ab06668f67cbf078d7a063086a0
raptazure <raptazure@foxmail.com>
2020-10-25 17:26:57 +0800

    haskell: format

--- haskell/ch04/Main.hs
+++ haskell/ch04/Main.hs
@@ -3,95 +3,101 @@ lucky 7 = "lucky number seven!"
 lucky _ = "sorry, you're out of luck"
 
 sayMe :: (Integral a) => a -> String
-sayMe 1 = "One!"  
-sayMe 2 = "Two!"  
-sayMe 3 = "Three!"  
-sayMe 4 = "Four!"  
-sayMe 5 = "Five!"  
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
 sayMe _ = "Not between 1 and 5"
 
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 length' :: Num a => [a] -> a
 length' [] = 0
-length' (_:xs) = 1 + length' xs
+length' (_ : xs) = 1 + length' xs
 
 length'' :: Num a => [a] -> a
-length'' x = sum [1 | _ <- x] 
+length'' x = sum [1 | _ <- x]
 
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
-head' (x:_) = x
+head' (x : _) = x
 
-tell :: (Show a) => [a] -> String  
-tell [] = "The list is empty"  
-tell (x:[]) = "The list has one element: " ++ show x  
-tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
-tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x : []) = "The list has one element: " ++ show x
+tell (x : y : []) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
 
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
-sum' (x:xs) = x + sum' xs
+sum' (x : xs) = x + sum' xs
 
 capital :: String -> String
 capital "" = "empty string"
-capital all'@(x:_) = "The first letter of " ++ all' ++ " is " ++ [x]
+capital all'@(x : _) = "The first letter of " ++ all' ++ " is " ++ [x]
 
-bmiTell :: (RealFloat a) => a -> a -> String  
-bmiTell weight height  
-    | bmi <= skinny = "You're underweight, you emo, you!"  
-    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
-    | otherwise     = "You're a whale, congratulations!"  
-    where bmi = weight / height ^ 2  
-          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where
+    bmi = weight / height ^ 2
+    (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 max' :: (Ord a) => a -> a -> a
-max' a b 
-    | a > b = a 
-    | otherwise = b
+max' a b
+  | a > b = a
+  | otherwise = b
 
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
-    | a > b = GT
-    | a == b = EQ
-    | otherwise = LT
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-    where (f:_) = firstname
-          (l:_) = lastname
+  where
+    (f : _) = firstname
+    (l : _) = lastname
 
-calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
-calcBmis xs = [bmi w h | (w, h) <- xs] 
-    where bmi weight height = weight / height ^ 2
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi weight height = weight / height ^ 2
 
-calcBmis' :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25]
 
-cylinder :: (RealFloat a) => a -> a -> a  
+cylinder :: (RealFloat a) => a -> a -> a
 -- let is an expression
-cylinder r h = 
-    let sideArea = 2 * pi * r * h  
-        topArea = pi * r ^2  
-    in  sideArea + 2 * topArea
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
 
--- 4 * (let a = 9 in a + 1) + 2 
--- [let square x = x * x in (square 5, square 3, square 2)] 
+-- 4 * (let a = 9 in a + 1) + 2
+-- [let square x = x * x in (square 5, square 3, square 2)]
 -- (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
 
-describeList :: [a] -> String  
-describeList xs = "The list is " ++ case xs of [] -> "empty."  
-                                               [_] -> "a singleton list."   
-                                               xs -> "a longer list."
+describeList :: [a] -> String
+describeList xs =
+  "The list is " ++ case xs of
+    [] -> "empty."
+    [_] -> "a singleton list."
+    xs -> "a longer list."
 
-describeList' :: [a] -> String  
-describeList' xs = "The list is " ++ what xs  
-    where what [] = "empty."  
-          what [_] = "a singleton list."  
-          what xs = "a longer list."
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+  where
+    what [] = "empty."
+    what [_] = "a singleton list."
+    what xs = "a longer list."
