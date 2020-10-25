18d2356010e5c5399bd6bba87ff7f3b241121cf9
raptazure <raptazure@foxmail.com>
2020-10-25 17:23:26 +0800

    haskell: ch04

--- haskell/ch04/Main.hs
+++ haskell/ch04/Main.hs
@@ -41,3 +41,57 @@ sum' (x:xs) = x + sum' xs
 capital :: String -> String
 capital "" = "empty string"
 capital all'@(x:_) = "The first letter of " ++ all' ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b 
+    | a > b = a 
+    | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b = GT
+    | a == b = EQ
+    | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs] 
+    where bmi weight height = weight / height ^ 2
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25]
+
+cylinder :: (RealFloat a) => a -> a -> a  
+-- let is an expression
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea
+
+-- 4 * (let a = 9 in a + 1) + 2 
+-- [let square x = x * x in (square 5, square 3, square 2)] 
+-- (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
+
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [_] -> "a singleton list."   
+                                               xs -> "a longer list."
+
+describeList' :: [a] -> String  
+describeList' xs = "The list is " ++ what xs  
+    where what [] = "empty."  
+          what [_] = "a singleton list."  
+          what xs = "a longer list."
