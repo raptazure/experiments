f02b20eccafabf31e60d7ba64e638687022b9d18
raptazure <raptazure@foxmail.com>
2020-10-27 12:22:46 +0800

    haskell: ch07e fix name

--- haskell/ch07e/app/Main.hs
+++ haskell/ch07e/app/Main.hs
@@ -3,11 +3,10 @@ module Main where
 import Geometry
 import GeometryFolder.Cube as Cube
 
-myIntToStr :: Float -> String
-myIntToStr x
-    | x < 3     = show x ++ " is less than three"
-    | otherwise = show x ++ " is bigger than three"
+myFloatToStr :: Float -> String
+myFloatToStr x
+  | x < 3 = show x ++ " is less than three"
+  | otherwise = show x ++ " is bigger than three"
 
-
-main :: IO()
-main = putStr $ myIntToStr $ Cube.volume 3
+main :: IO ()
+main = putStr $ myFloatToStr $ Cube.volume 3
