5ce9528c12dfb2d97a36e73ab551e9f39c7fd507
raptazure <raptazure@foxmail.com>
2020-11-30 20:58:12 +0800

    haskell: init monad

--- haskell/wiwinwlh/monads/src/Lib.hs
+++ haskell/wiwinwlh/monads/src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b
+
